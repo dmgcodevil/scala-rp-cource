f5d48b530a79a52bc843d3bc153b02db410cda0b
roman_pleshkov <Roman.Pleshkov@united-security-providers.ch>
2015-05-07 18:20:34 +0300

    w4

---
+++ src/main/scala/com/dmgcodevil/rx/CreateObservable.scala
@@ -0,0 +1,14 @@
+package com.dmgcodevil.rx
+
+import rx.lang.scala.{Subscription, Observer, Observable}
+
+/**
+ * @author roman_pleshkov
+ */
+object CreateObservable {
+
+  def main(args: Array[String]) {
+    //Observable.cre
+  }
+
+}
